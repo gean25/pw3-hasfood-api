34d15776dc1e6a3be2181cda51b3de9efb948c11
Gean <gean@test.com>
2022-03-30 13:36:17 -0300

    Criacao do projeto

---
+++ src/test/java/br/com/gean/pedro/pw3hasfoodapi/Pw3HasfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gean.pedro.pw3hasfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3HasfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
